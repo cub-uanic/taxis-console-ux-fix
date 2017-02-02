0bb4169045415b2df02963784d932300d2b0a783
Oleg Kostyuk <cub.uanic@gmail.com>
2017-02-02 13:37:30 +0200

    Create tampermonkey.taxis-ux.js

---
+++ tampermonkey.taxis-ux.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         TaxisConsole
+// @namespace    https://github.com/cub-uanic/taxis-console-ux-fix
+// @version      0.2
+// @description  improve UX
+// @author       Oleg Kostyuk <cub.uanic@gmail.com>
+// @match        http://taxis.agora.odesk.com/*
+// @match        https://taxis.agora.odesk.com/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function main($){
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = '.panel-default>.panel-heading {background-color:#ccc !important}';
+        head.appendChild(style);
+    }
+
+    main();
+})();
